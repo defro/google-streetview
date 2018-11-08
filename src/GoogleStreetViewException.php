da3c22ee27071c7b5b1c32b2d9acfc19d7bd0f35
Joël Gaujard <j.gaujard@laposte.net>
2018-11-08 23:31:15 +0100

    CI tools and other configurations

--- src/GoogleStreetViewException.php
+++ src/GoogleStreetViewException.php
@@ -7,9 +7,9 @@ use Throwable;
 class GoogleStreetViewException extends \Exception
 {
 
-    public function __construct(string $message = '', int $code = 0, Throwable $previous = null)
+    public function __construct(string $message, int $code = null, Throwable $previous = null)
     {
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, (int) $code, $previous);
     }
 
 }
