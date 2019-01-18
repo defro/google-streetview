d6584bb4dad404eb98053735b670cd48ff4a7fc7
Joël Gaujard <j.gaujard@laposte.net>
2019-01-18 01:04:34 +0100

    Scrutinizer code analysis fix

--- src/Exception/RequestException.php
+++ src/Exception/RequestException.php
@@ -2,11 +2,9 @@
 
 namespace Defro\Google\StreetView\Exception;
 
-use Throwable;
-
 class RequestException extends \RuntimeException
 {
-    public function __construct(string $message, Throwable $previous = null)
+    public function __construct(string $message, \Exception $previous = null)
     {
         parent::__construct($message, 0, $previous);
     }
